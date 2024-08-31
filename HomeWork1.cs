74b305ef338f1405e64c3e67ab2cd3d3788a580f
Mportillo1108 <mmportillo1@buffs.wtamu.edu>
2024-08-31 13:50:21 -0500

    Add files via upload

---
+++ HomeWork1.cs
@@ -0,0 +1,14 @@
+﻿namespace HW1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Code for Homework 1
+        double X = 2.5;
+        double Y = 3.3;
+        double Z = 4*X*X + 3*Y;
+        Console.WriteLine(Z);
+        
+    }
+}
